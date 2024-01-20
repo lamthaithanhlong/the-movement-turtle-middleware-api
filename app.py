0cded474edf0b81fa42016a32d3065c6d3731fda
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2024-01-20 10:15:37 -0600

    update ros_endpoint

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from flask import Flask, request
 from flask_cors import CORS
 import requests
@@ -8,18 +7,18 @@ CORS(app)
 
 @app.route('/move_robot', methods=['POST'])
 def move_robot():
-    command = request.json
-    # Replace with your ROS server IP and port
-    ros_endpoint = "https://i-07cf7ce47bac87c9f.robotigniteacademy.com/5496ab72-2516-4738-a009-317754113163/webpage/"
+    data = request.json
+    command = data.get('command')
+    ros_endpoint = data.get('ros_endpoint')  # Get the dynamic ROS endpoint from the request
+
+    if not command or not ros_endpoint:
+        return "Bad request, missing data", 400
 
     try:
         response = requests.post(ros_endpoint, json=command)
-        # Return the same status code and text received from ROS server
         return response.text, response.status_code
     except requests.exceptions.RequestException as e:
-        # Return a server error message if the ROS server is unreachable
-        print("Internal server")
+        return f"Error contacting ROS server: {e}", 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
